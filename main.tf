14217ff975a9ed6065150829d587fb82d8ec4de5
Karuna0909 <karunatiwari0909@gmail.com>
2025-09-04 18:50:28 +0530

    added new eg

--- main.tf
+++ main.tf
@@ -3,7 +3,3 @@ resource "azurerm_resource_group" "Butterfly" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "Butterfly1" {
-  name     = "Rg012-resources"
-  location = "West Europe"
-}

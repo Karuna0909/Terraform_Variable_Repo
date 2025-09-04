601b8b6ae3d9f9ece2a46312bb249afb0aab8e3d
Karuna0909 <karunatiwari0909@gmail.com>
2025-09-04 18:45:26 +0530

    added new reg

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "Butterfly" {
   name     = "Rg01-resources"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "Butterfly1" {
+  name     = "Rg012-resources"
+  location = "West Europe"
+}
